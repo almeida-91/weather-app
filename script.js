bd1158375b06767c4c7c45ffb0ee9eddd6cee57b
Almeida-91 <aplalmeida91@outlook.com>
2022-08-31 22:45:50 +0100

    Add gif related to searched location Change CSS styling

--- script.js
+++ script.js
@@ -31,27 +31,31 @@ async function getWeather(location) {
 const locationInput = document.getElementsByTagName('input')[0];
 const submitButton = document.getElementsByTagName('button')[0];
 const contentDiv = document.getElementById('content');
+contentDiv.style.display = 'none';
 let currentWeather;
-displayCredit('https://unsplash.com/@kelvinyan');
+changeBackground('initial');
 
 submitButton.addEventListener('click', () => {
     getWeather(locationInput.value);
-    displayCredit('https://unsplash.com/@kelvinyan');
 });
 
-function displayInfo(weather) {
+async function displayInfo(weather) {
     const location = document.createElement('h3');
     location.textContent = locationInput.value;
     clearInput();
     clearDisplay();
     if (weather == 'error'){
-        location.textContent = 'NOT FOUND :(';
+        location.textContent = 'NOT FOUND :( (check spelling)';
         contentDiv.appendChild(location);
+        contentDiv.style.display = 'grid';
+        getGif('reading');
         return;
     } else if (weather == '') {
         location.textContent = 'Please enter a city!';
         location.style.textAlign = 'center';
         contentDiv.appendChild(location);
+        contentDiv.style.display = 'grid';
+        getGif('what');
         return;
     }
     let feelsLike = document.createElement('span');
@@ -72,13 +76,15 @@ function displayInfo(weather) {
     minImage.src = './images/snowflake-thermometer.png';
     humidImage.src = './images/cloud-percent-outline.png';
     
-    feelsLike.textContent = `Feels like : ${weather.feels_like}`;
-    temp.textContent = `Temperature : ${weather.temp}`;
-    tempmax.textContent = `Max : ${weather.temp_max}`;
-    tempmin.textContent = `Min : ${weather.temp_min}`;
-    humidity.textContent = `Humidity : ${weather.humidity}`;
+    feelsLike.textContent = `Feels like : ${weather.feels_like}°C`;
+    temp.textContent = `Temperature : ${weather.temp}°C`;
+    tempmax.textContent = `Max : ${weather.temp_max}°C`;
+    tempmin.textContent = `Min : ${weather.temp_min}°C`;
+    humidity.textContent = `Humidity : ${weather.humidity}%`;
 
     contentDiv.appendChild(location);
+    await getGif(location.textContent);
+
     contentDiv.appendChild(feelsImage);
     contentDiv.appendChild(feelsLike);
     contentDiv.appendChild(tempImage);
@@ -89,6 +95,8 @@ function displayInfo(weather) {
     contentDiv.appendChild(tempmin);
     contentDiv.appendChild(humidImage);
     contentDiv.appendChild(humidity);
+    contentDiv.style.display = 'grid';
+    changeBackground(weather.temp);
 }
 
 
@@ -100,13 +108,62 @@ function clearInput() {
     locationInput.value = '';
 }
 
-function displayCredit(creditInfo){
-    const credit = document.getElementById('credit');
-    credit.innerHTML = '';
-    credit.textContent += 'Photo taken by';
+function changeBackground(temp) {
+    if (temp == 'initial'){
+        document.body.style.backgroundImage = 'URL(./images/kelvin-yan-WWl6xaSHFIo-unsplash.jpg)';
+        document.body.style.backgroundSize = 'cover';
+        giveCredit('initial');
+        return;
+    } else if (temp >= 25) {
+        document.body.style.backgroundImage = 'URL(./images/elizeu-dias-RN6ts8IZ4_0-unsplash.jpg)';
+    } else if (temp > 0 && temp < 10) {
+        document.body.style.backgroundImage = 'URL(./images/osman-rana-HOtPD7Z_74s-unsplash.jpg)';
+    } else if (temp > 10 && temp < 25){
+        document.body.style.backgroundImage = 'URL(./images/todd-kent-jqXiBG2VDeA-unsplash.jpg)';
+    } else if (temp < 0) {
+        document.body.style.backgroundImage = 'URL(./images/fabian-mardi-kVKz9qnJC-k-unsplash.jpg)';
+    }
 
+    giveCredit(temp);
+    document.body.style.backgroundSize = 'cover';
+}
+
+function giveCredit(temp) {
+    const credit = document.getElementById('credit');
     const creditLink = document.createElement('a');
-    creditLink.textContent = 'this guy';
-    creditLink.href = creditInfo;
+
+    credit.innerHTML = '';
+    credit.textContent = 'Photo taken by ';
+
+    if (temp == 'initial') {
+        creditLink.href = 'https://unsplash.com/@kelvinyan';
+        creditLink.textContent = 'Kelvin Yan';
+    } else if (temp >= 25) {
+        creditLink.href = 'https://unsplash.com/@elishavision';
+        creditLink.textContent = 'Elizeu Dias';    
+    } else if (temp > 0 && temp < 10) {
+        creditLink.href = 'https://unsplash.com/@osmanrana';
+        creditLink.textContent = 'Osman Rana';
+    } else if (temp > 10 && temp < 25){
+        creditLink.href = 'https://unsplash.com/@churchoftodd';
+        creditLink.textContent = 'Todd Kent';
+    } else if (temp < 0 ) {
+        creditLink.href = 'https://unsplash.com/@fabianmardi';
+        creditLink.textContent = 'Fabian Mardi';
+    }
     credit.appendChild(creditLink);
+}
+
+async function getGif(location){
+    let img = document.createElement('img');
+    img.className = 'gif';
+    try{
+        const response  = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=AGFTIsyFul6yjnSGlXz8A4ALRB8a8YrD&s=${location}`, {mode: 'cors'})
+        const data = await response.json();
+        img.src = data.data.images.original.url;
+        img.style.borderRadius = '20px';
+        contentDiv.appendChild(img);
+    } catch (error){
+        console.error(error);
+    }
 }
